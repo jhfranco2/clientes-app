bb81d22808464044ae063389adf53e333b2943f6
jfranco <jhoanmateofranco@gmail.com>
2022-10-24 11:27:41 -0500

    agregando el boton para eliminar

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import swal from 'sweetalert2';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
 
@@ -14,8 +15,38 @@ export class ClientesComponent implements OnInit {
 
   ngOnInit() {
     this.clienteService.getClientes().subscribe(
-       clientes => this.clientes = clientes
+      clientes => this.clientes = clientes
     );
+  }
+
+  delete(cliente: Cliente): void {
+   swal({
+    title:'Estas seguro?',
+    text: `¿Seguro que desea eliminar al cliente ${cliente.nombre} ${cliente.apellido}`,
+    type: 'warning',
+    showCancelButton:true,
+    confirmButtonColor:'#3085d6',
+    cancelButtonColor:'#d33',
+    confirmButtonText:'Si, eliminalo',
+    cancelButtonText:'No, cancelar',
+    cancelButtonClass:'btn btn-danger',
+    buttonsStyling:false,
+    reverseButtons:true
+   }).then((result)=>{
+      if(result.value){
+        this.clienteService.delete(cliente.id).subscribe(
+          response =>{
+            this.clientes = this.clientes.filter(cli => cli !== cliente)
+            swal(
+              'Eliminado',
+              `Cliente ${cliente.nombre} eliminado con exito`,
+              'success'
+             )
+          }
+        )
+         
+      }
+   })
   }
 
 }
