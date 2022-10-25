feb2db806c596406430b07b04cd4480204f16062
jfranco <jhoanmateofranco@gmail.com>
2022-10-25 14:28:58 -0500

    Control de errores angular

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
-import { Observable } from 'rxjs';
-import { of } from 'rxjs';
+import { of, Observable, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
+import swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 
 @Injectable()
@@ -12,8 +13,8 @@ export class ClienteService {
   private urlEndPoint: string = 'http://localhost:8080/api/clientes';
 
 
-  private httpHeaders = new HttpHeaders({'Content-Type':'application/json'})
-  constructor(private http: HttpClient) { }
+  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' })
+  constructor(private http: HttpClient, private router:Router) { }
 
   getClientes(): Observable<Cliente[]> {
     //return of(CLIENTES);
@@ -22,19 +23,44 @@ export class ClienteService {
     );
   }
 
-  create(cliente:Cliente): Observable<Cliente>{
-     return this.http.post<Cliente>(this.urlEndPoint,cliente,{headers: this.httpHeaders});
+  create(cliente: Cliente): Observable<Cliente> {
+    return this.http.post(this.urlEndPoint, cliente, { headers: this.httpHeaders }).pipe(
+      map((response: any) => response.cliente as Cliente),
+      catchError(e => {
+        swal(e.error.mensaje, e.error.error, 'error');
+        return throwError(() => new Error(e));
+      })
+    );
   }
 
-  getCliente(id:number): Observable<Cliente>{
-    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`);
-  }
-  
-  update(cliente:Cliente): Observable<Cliente>{
-    return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`,cliente,{headers: this.httpHeaders});
+  getCliente(id: number): Observable<Cliente> {
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
+      catchError(e => {
+        this.router.navigate(['/cliente']);
+        console.error(e.error.mensaje);
+        swal(e.error.mensaje, e.error.error, 'error');
+
+        return throwError(() => new Error(e));
+      })
+    );
   }
 
-  delete(id:number):Observable<Cliente>{
-    return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`,{headers:this.httpHeaders});
+  update(cliente: Cliente): Observable<any> {
+    return this.http.put<any>(`${this.urlEndPoint}/${cliente.id}`, cliente, { headers: this.httpHeaders }).pipe(
+      catchError(e => {
+        swal(e.error.mensaje, e.error.error, 'error');
+        return throwError(() => new Error(e));
+      })
+    );
+  }
+
+  delete(id: number): Observable<Cliente> {
+    return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, { headers: this.httpHeaders }).pipe(
+      catchError(e => {
+        console.error(e.error.mensaje);
+        swal(e.error.mensaje, e.error.error, 'error');
+        return throwError(() => new Error(e));
+      })
+    );
   }
 }
--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
-import { Router,ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { ThisReceiver } from '@angular/compiler';
 import swal from 'sweetalert2';
 
@@ -11,45 +11,43 @@ import swal from 'sweetalert2';
 })
 export class FormComponent implements OnInit {
 
-  public cliente:Cliente = new Cliente();
+  public cliente: Cliente = new Cliente();
 
-  public titulo:string = "Crear Cliente";
-  constructor(private clienteService: ClienteService, private router:Router, 
-    private activeRouter:ActivatedRoute) { }
+  public titulo: string = "Crear Cliente";
+  constructor(private clienteService: ClienteService, private router: Router,
+    private activeRouter: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.cargarCliente()
     this.update()
   }
-  
-    cargarCliente():void{
-    this.activeRouter.params.subscribe(params=>{
+
+  cargarCliente(): void {
+    this.activeRouter.params.subscribe(params => {
       let id = params['id']
-      if(id){
-        this.clienteService.getCliente(id).subscribe( (cliente) => this.cliente = cliente)
+      if (id) {
+        this.clienteService.getCliente(id).subscribe((cliente) => this.cliente = cliente)
       }
     })
   }
-  public create(): void{
-    this.clienteService.create(this.cliente).subscribe(
-      response => {
+  public create(): void {
+    this.clienteService.create(this.cliente)
+    .subscribe(cliente => {
         this.router.navigate(['/clientes'])
-        swal('Nuevo Cliente', `Cliente ${this.cliente.nombre} creado con exito`,'success')
+        swal('Nuevo Cliente', `El cliente ${cliente.nombre} ha sido creado con éxito`, 'success')
       }
-      
+
     );
-    
+
   }
 
-  update():void{
-    console.log("Entrando a actualizar  metodo")
+  update(): void {
     this.clienteService.update(this.cliente)
-    .subscribe(cliente => 
-      {
-          this.router.navigate(['/clientes'])
-          swal('Cliente Actualizado',`Cliente ${cliente.nombre} actualizado con éxito! ` , 'success')
-        }
-      
-    );
+      .subscribe(json => {
+        this.router.navigate(['/clientes'])
+        swal('Cliente Actualizado', `${json.mensaje}: ${json.cliente.apellido}`, 'success')
+      }
+
+      );
   }
 }
